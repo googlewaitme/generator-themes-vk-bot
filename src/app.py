7b70f63915688bb7484e894ab1fa0505f0e8cbef
googlewaitme <kusok.apelbsina@hotmail.com>
2023-01-16 01:27:05 +0300

    chore

--- src/app.py
+++ src/app.py
@@ -1,21 +1,18 @@
 import logging
 
-from vkwave.bots import SimpleLongPollBot, SimpleBotEvent
+from vkwave.bots import SimpleLongPollBot
 
 import redis
 
 from da_vinchi import DaVinchi
 import config
-import messages
-from utils import constants 
 from middlewares.user_data_middleware import UserMiddleware
 from middlewares.spam_control import SpamControlMiddleware
 
-from blueprints import (menu_router, 
-						back_router, 
-						new_article_router,
-						new_content_plan_router
-)
+from blueprints import (menu_router,
+                        back_router,
+                        new_article_router,
+                        new_content_plan_router)
 from db import models
 
 
@@ -25,9 +22,9 @@ bot = SimpleLongPollBot(tokens=config.VK_TOKEN, group_id=config.VK_GROUP_ID)
 dv = DaVinchi(config.OPEN_AI_TOKEN)
 
 spam_controller = redis.Redis(
-	host=config.REDIS_HOST,
-	port=config.REDIS_PORT,
-	db=1
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    db=1
 )
 
 models.create_tables()
--- src/da_vinchi.py
+++ src/da_vinchi.py
@@ -4,49 +4,49 @@ import messages
 
 
 class DaVinchi:
-	def __init__(self, OPEN_AI_TOKEN):
-		openai.api_key = OPEN_AI_TOKEN
-		self.model_name = 'text-davinci-003'
+    def __init__(self, OPEN_AI_TOKEN):
+        openai.api_key = OPEN_AI_TOKEN
+        self.model_name = 'text-davinci-003'
 
-	def get_content_plan_by_theme(self, theme: str) -> str:
-		response = openai.Completion.create(
-			model=self.model_name,
-			prompt=templates.GET_CONTENT_PLAN_TEMPLATE.format(theme=theme),
-			max_tokens=3000,
-			temperature=0.3,
-			top_p=1.0,
-			frequency_penalty=0.1,
-			presence_penalty=0.1,
-			stop=['16.']
-		)
-		if len(response['choices']) == 0:
-			return messages.BAD_RESULTAT_MESSAGE
-		return response['choices'][0]['text']
+    def get_content_plan_by_theme(self, theme: str) -> str:
+        response = openai.Completion.create(
+            model=self.model_name,
+            prompt=templates.GET_CONTENT_PLAN_TEMPLATE.format(theme=theme),
+            max_tokens=2000,
+            temperature=0.3,
+            top_p=1.0,
+            frequency_penalty=0.1,
+            presence_penalty=0.1,
+            stop=['16.']
+        )
+        if len(response['choices']) == 0:
+            return messages.BAD_RESULTAT_MESSAGE
+        return response['choices'][0]['text']
 
-	def get_atricle_by_theme(self, theme: str) -> str:
-		response = openai.Completion.create(
-			model=self.model_name,
-			prompt=templates.GET_ARTICLE_BY_THEME_TEMPLATE.format(theme=theme),
-			temperature=0.3,
-			max_tokens=3000,
-			top_p=1.0,
-			frequency_penalty=0.0,
-			presence_penalty=0.0
-		)
-		if len(response['choices']) == 0:
-			return messages.BAD_RESULTAT_MESSAGE
-		return response['choices'][0]['text']
+    def get_atricle_by_theme(self, theme: str) -> str:
+        response = openai.Completion.create(
+            model=self.model_name,
+            prompt=templates.GET_ARTICLE_BY_THEME_TEMPLATE.format(theme=theme),
+            temperature=0.3,
+            max_tokens=2000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+        if len(response['choices']) == 0:
+            return messages.BAD_RESULTAT_MESSAGE
+        return response['choices'][0]['text']
 
-	def send_question(self, question: str) -> str:
-		response = openai.Completion.create(
-			model=self.model_name,
-			prompt=f"{question}",
-			temperature=0.3,
-			max_tokens=3000,
-			top_p=1.0,
-			frequency_penalty=0.0,
-			presence_penalty=0.0
-		)
-		if len(response['choices']) == 0:
-			return messages.BAD_RESULTAT_MESSAGE
-		return response['choices'][0]['text']
+    def send_question(self, question: str) -> str:
+        response = openai.Completion.create(
+            model=self.model_name,
+            prompt=f"{question}",
+            temperature=0.3,
+            max_tokens=2000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+        if len(response['choices']) == 0:
+            return messages.BAD_RESULTAT_MESSAGE
+        return response['choices'][0]['text']
