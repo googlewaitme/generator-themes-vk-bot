946088cf0276f86f6fa17d0ef7119f7aeb59f834
googlewaitme <kusok.apelbsina@hotmail.com>
2023-01-22 20:28:20 +0300

    added sentry

--- src/config.py
+++ src/config.py
@@ -8,6 +8,11 @@ VK_GROUP_ID = env.str('VK_GROUP_ID')
 VK_GROUP_NAME = env.str('VK_GROUP_NAME', "public" + VK_GROUP_ID)
 OPEN_AI_TOKEN = env.str('OPEN_AI_TOKEN')
 
+# --- Sentry SDK ---
+
+SENTRY_SDK_RUN = env.bool("SENTRY_SDK_RUN", False)
+if SENTRY_SDK_RUN:
+    SENTRY_SDK_TOKEN = env.str("SENTRY_SDK_TOKEN")
 
 # --- DATABASE ---
 
@@ -18,6 +23,7 @@ DATABASE_NAME = env.str("DATABASE_NAME", "users.db")
 REDIS_HOST = env.str("REDIS_HOST", "localhost")
 REDIS_PORT = env.int("REDIS_PORT", 6379)
 
-# --- SPAM CONTROL --- 
+# --- SPAM CONTROL ---
 
-LIMIT_NUMBER_OF_MESSAGES_PER_HOUR = env.int("LIMIT_NUMBER_OF_MESSAGES_PER_HOUR", 90)
+LIMIT_NUMBER_OF_MESSAGES_PER_HOUR = env.int(
+    "LIMIT_NUMBER_OF_MESSAGES_PER_HOUR", 90)
--- src/loader.py
+++ src/loader.py
@@ -1,5 +1,12 @@
 from da_vinchi import DaVinchi
 import config
+import sentry_sdk
 
 
 dv = DaVinchi(config.OPEN_AI_TOKEN)
+
+if config.SENTRY_SDK_RUN:
+    sentry_sdk.init(
+        dsn=config.SENTRY_SDK_TOKEN,
+        traces_sample_rate=1.0
+    )
