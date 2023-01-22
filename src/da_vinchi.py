3bd13677475fb09155872b2fb7174d7ef8cdd9a2
googlewaitme <kusok.apelbsina@hotmail.com>
2023-01-23 00:19:27 +0300

    added working with errors from open-ai

--- src/da_vinchi.py
+++ src/da_vinchi.py
@@ -1,6 +1,27 @@
 import openai
 import templates
 import messages
+import time
+
+from openai import error
+from sentry_sdk import capture_exception
+
+
+def openai_errors(func):
+    def _wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except error.ServiceUnavailableError:
+            capture_exception(error.ServiceUnavailableError())
+            return messages.SERVICE_IS_UNAVAILABLE
+        except error.RateLimitError:
+            capture_exception(error.RateLimitError())
+            time.sleep(31)
+            return _wrapper(*args, **kwargs)
+        except Exception as e:
+            capture_exception(e)
+            return messages.UNEXCEPTED_ERROR
+    return _wrapper
 
 
 class DaVinchi:
@@ -8,6 +29,7 @@ class DaVinchi:
         openai.api_key = OPEN_AI_TOKEN
         self.model_name = 'text-davinci-003'
 
+    @openai_errors
     def get_content_plan_by_theme(self, theme: str) -> str:
         response = openai.Completion.create(
             model=self.model_name,
